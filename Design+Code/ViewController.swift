82aa9e21e6b09c3c980bcbf6272e1b961e4be68d
Mikail <mikailbaykara14@gmail.com>
2022-05-13 19:54:10 +0300

    first commit after first 5 video

---
+++ Design+Code/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Design+Code
+//
+//  Created by Mikail Baykara on 12.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var viewCard: UIView!
+    @IBOutlet var blurView: UIVisualEffectView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
